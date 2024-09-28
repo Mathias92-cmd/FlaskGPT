7bd7365b2286ab18eba34f0e25b4cac6494df52f
MOREL Mathias <mathias_morel@ens.univ-artois.fr>
2024-09-28 09:28:05 +0200

    methode conversation

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-from flask import Flask , render_template
+import os
+import openai
+from dotenv import load_dotenv
+from flask import Flask, render_template
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
 
@@ -6,6 +12,12 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+def build_conversation_dict(messages: list) -> list[dict]:
+    return [
+        {"role" : "user" if i % 2 == 0 else "assistant" , "content":message}
+        for i , message in enumerate(messages)
+    ]
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1' , port=5000)
